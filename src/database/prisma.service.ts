84295436b90de3de691fda66c321237102098108
Kharis Fadillah <kharisf@gmail.com>
2026-06-08 05:58:49 +0800

    wip: setup prisma schema, adapter, and database module

---
+++ src/database/prisma.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client.js';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    const adapter = new PrismaPg(connectionString as string);
+
+    super({
+      adapter,
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
